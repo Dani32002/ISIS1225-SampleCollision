bd954cbaaa4af12b0d8e789ec8f50cc8da5fde46
Dani <d.escalante@uniandes.edu.co>
2022-03-21 16:52:37 -0500

    Paso 6 parte 1

--- App/model.py
+++ App/model.py
@@ -314,8 +314,10 @@ def sortBooksByYear(catalog, year, rank):
 
         # ordenando la lista
         sorted_list = sa.sort(books_year, compareratings)
-        ranked_list = lt.subList(sorted_list, 1, rank)
-
+        if rank < lt.size(sorted_list):
+            ranked_list = lt.subList(sorted_list, 1, rank)
+        else:
+            ranked_list = lt.subList(sorted_list, 1, lt.size(sorted_list))
     return ranked_list
 
 
